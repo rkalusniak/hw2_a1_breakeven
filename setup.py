dbb57e67fac63094746580bebc0c73d517c35c1e
rkalusniak <rachel.kalusniak@gmail.com>
2023-06-10 14:26:26 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='whatif',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=['numpy', 'pandas'],
+    version='0.1.0',
+    description='Whatif analysis in Python',
+    author='misken',
+    license='',
+)
